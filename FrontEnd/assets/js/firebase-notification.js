90cab85e5f489454c478f132521145b05b6c4ecb
Karl Eljun <eljunkarl.atay@nmsc.edu.ph>
2026-05-06 22:55:49 +0800

    E-commerce Improvements Phase 5 Firebase Cloud Messaging

---
+++ FrontEnd/assets/js/firebase-notification.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBg_xIbb74aWye2s0cKF8SfyIBFYBZjy94",
+  authDomain: "e-commerce-45367.firebaseapp.com",
+  projectId: "e-commerce-45367",
+  storageBucket: "e-commerce-45367.firebasestorage.app",
+  messagingSenderId: "100656810885",
+  appId: "1:100656810885:web:0ae522bd849f6d850352ec",
+  measurementId: "G-97XLJS4249",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
